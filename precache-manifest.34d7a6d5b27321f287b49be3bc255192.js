9b1ef9175ab13887b80f6956649df31535c872c3
Akshay Shahi <shahiakshay31@gmail.com>
2020-12-28 21:09:51 +0530

    build progressss

---
+++ precache-manifest.34d7a6d5b27321f287b49be3bc255192.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d8d49031e12637a77add",
+    "url": "/static/js/main.d8d49031.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d8d49031e12637a77add",
+    "url": "/static/css/main.f9b07c9f.chunk.css"
+  },
+  {
+    "revision": "4fab8cd1a33859b2861c6f88993099d4",
+    "url": "/index.html"
+  }
+];
